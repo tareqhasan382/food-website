aa6b1c3d35b8c21098acc5d4242425b515f8a588
tareqhasan382 <tareqhasan382@gmail.com>
2023-11-25 19:14:35 +0600

    routes implement

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
@@ -9,9 +8,7 @@ import routes from "./routes/index.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={routes}>
-        <App />
-      </RouterProvider>
+      <RouterProvider router={routes} />
     </Provider>
   </React.StrictMode>
 );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-// import App from "../App";
-//import Hero from "../components/Hero";
 import App from "../App";
 import Cart from "../components/Cart";
 const routes = createBrowserRouter([
